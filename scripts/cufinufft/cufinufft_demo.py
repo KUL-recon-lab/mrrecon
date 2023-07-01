63d2bfba36834f253a0a0e8855f9430ee9666b3b
Georg <gschra2@stanford.edu>
2023-06-30 18:03:10 -0700

    improve sigpy vs cufinufft comparison

--- scripts/cufinufft/cufinufft_demo.py
+++ scripts/cufinufft/cufinufft_demo.py
@@ -12,13 +12,13 @@ from time import time
 num_rep = 1
 
 dtype = np.float32
-shape = (64, 64, 64)
-M = int(1e3)
+shape = (256, 256, 256)
+M = int(1e7)
 tol = 1e-6
 complex_dtype = utils._complex_dtype(dtype)
 
 # sign in exp.
-isign = -1
+isign = 1
 
 # setup M random real 3-d kspace points within [-pi, pi)^3
 k = utils.gen_nu_pts(M).astype(dtype)
@@ -40,7 +40,7 @@ plan2.set_pts(k_gpu[0], k_gpu[1], k_gpu[2])
 plan1 = cufinufft(1, shape, eps=tol, dtype=dtype, isign = isign)
 plan1.set_pts(k_gpu[0], k_gpu[1], k_gpu[2])
 
-ind = 0
+ind = 42
 
 # execute type2 NUFFT (forward step going from regular image to non-unif. kspace)
 t0 = time()
@@ -61,22 +61,43 @@ plan1.execute(c_gpu, fk_gpu)
 res1 = fk_gpu.get()
 t3 = time()
 
+#----------------------------------------------------------------------
+
+import sigpy
+import cupy as cp
+
+# sigpy expects the k-space coordiantes in a transposed way
+# and ranging from -N/2 ... (N/2 - 1)
+coords = k.T.copy()
+for i in range(len(shape)):
+    coords[:,i] *= (0.5*shape[i]/np.pi)
+
+coords = cp.asarray(coords)
+fk_cupy = cp.asarray(fk)
+
+t4 = time()
+A = sigpy.linop.NUFFT(shape, coords)
+res2_sigpy = A(fk_cupy) 
+t5 = time()
+res2_sigpy *= np.sqrt(np.prod(shape)) 
+
+c_cupy = cp.asarray(c)
+t6 = time()
+res1_sigpy = A.H(c_cupy) 
+t7 = time()
+res1_sigpy *= np.sqrt(np.prod(shape)) 
+
+#----------------------------------------------------------------------
+
+
 print(f'<Ax,y>   : {(res2*np.conj(c)).sum():.3f}')
 print(f'<x,A^H y>: {(fk*np.conj(res1)).sum():.3f}')
-print(f't fwd: {(t1-t0):.5f}s')
-print(f't adj: {(t3-t2):.5f}s')
-print(f'type 2 rel err: {np.abs(res2_direct - res2[ind]) / np.abs(res2_direct)}')
+print(f'cufinufft t fwd: {(t1-t0):.5f}s')
+print(f'cufinufft t adj: {(t3-t2):.5f}s')
+print(f'sigpy     t fwd: {(t5-t4):.5f}s')
+print(f'sigpy     t adj: {(t7-t6):.5f}s')
+print(f'cufinufft rel err: {np.abs(res2_direct - res2[ind]) / np.abs(res2_direct)}')
+print(f'sigpy     rel err: {np.abs(res2_direct - res2_sigpy[ind]) / np.abs(res2_direct)}')
 print()
 
 
-import sigpy
-
-coords = k.T.copy()
-for i in range(3):
-    coords[:,i] *= (0.5*shape[i]/np.pi)
-
-A = sigpy.linop.NUFFT(shape, k.T)
-
-res2_sigpy = A(fk)
-
-print(np.abs(res2[:5]) / np.abs(res2_sigpy[:5]))
